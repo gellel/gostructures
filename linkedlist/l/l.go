08f58c38e3c67b314db250c1b4f03399657614ee
gellel <lindsay.gelle@gmail.com>
2019-01-02 23:32:24 +1100

    more linkedlist stuff

--- linkedlist/l/l.go
+++ linkedlist/l/l.go
@@ -1,5 +1,14 @@
-package l 
+package l
+
+import (
+	"github.com/gellel/gostructures/linkedlist/node/n"
+)
 
 type LinkedList interface {
-	Node interface{}
+	Add(property interface{}) *n.Node
+	Delete() int
+	Prepend(property interface{}) *n.Node
+	Reverse()
+	Search() bool
+	Traverse()
 }
