4d2739d137e5a69e54c2485e829656411fed2135
gellel <lindsay.gelle@gmail.com>
2019-01-03 16:50:47 +1100

    more linkedlist

--- linkedlist/linkedlist.go
+++ linkedlist/linkedlist.go
@@ -7,3 +7,36 @@ import (
 type LinkedList struct {
 	Node *node.Node
 }
+
+func New() *LinkedList {
+	return &LinkedList{}
+}
+
+func (LinkedList *LinkedList) Add(property interface{}) *LinkedList {
+
+	n := node.New(property)
+
+	if LinkedList.hasFirst() {
+		return LinkedList.addFirst(n)
+	}
+	return LinkedList.add(n)
+}
+
+func (linkedList *LinkedList) addFirst(n *node.Node) *LinkedList {
+
+	linkedList.Node = n
+
+	return linkedList
+}
+
+func (linkedList *LinkedList) add(n *node.Node) *LinkedList {
+	c := linkedList.Node
+
+	for !(c.Next == nil) {
+		c = c.Next
+	}
+}
+
+func (linkedList *LinkedList) hasFirst() bool {
+	return !(linkedList.Node == nil)
+}
--- linkedlist/node/node.go
+++ linkedlist/node/node.go
@@ -9,3 +9,7 @@ type Node struct {
 	Next  *Node
 	Value interface{}
 }
+
+func New(property interface{}) *Node {
+	return &Node{Value: property}
+}
