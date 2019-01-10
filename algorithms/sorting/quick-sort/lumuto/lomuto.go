2b94c1dadc04f434d94b7431116ea2985edc2179
gellel <lindsay.gelle@gmail.com>
2019-01-10 14:06:53 +1100

    working lomuto qs

--- algorithms/sorting/quick-sort/lumuto/lomuto.go
+++ algorithms/sorting/quick-sort/lumuto/lomuto.go
@@ -1,28 +1,37 @@
 package lomuto
 
-func Quicksort(unsorted *[]int) *[]int {
-	return quicksort(unsorted, 0, len(*unsorted)-1)
+
+// Quicksort algorithm using Lomuto partition scheme.
+// Uses floor: 0 and ceiling: n-1 to divide
+// and organise the unsorted Slice or Array.
+func Quicksort(a []int, floor int, ceiling int) []int {
+
+	if floor < ceiling {
+		partition := Partition(a, floor, ceiling)
+
+		Quicksort(a, floor, (partition - 1))
+		Quicksort(a, (partition + 1), ceiling)
+	}
+	return a
 }
 
-func quicksort(A *[]int, low int, high int) *[]int {
-	if low < high {
-		p := partition(A, low, high)
+func Partition(a []int, floor int, ceiling int) int {
+    
+    p := a[ceiling]
+    i := (floor - 1)
 
-		quicksort(A, low, p-1)
-		quicksort(A, p+1, high)
-	}
-	return A
+    for j := floor; j <= (ceiling - 1); j++ {
+        if a[j] <= p {
+            i = i + 1
+            Swap(a, i, j)
+        }
+    }
+    Swap(a, i + 1, ceiling)
+
+    return i + 1
 }
 
-func partition(A *[]int, low int, high int) int {
-	pivot := (*A)[high]
-	i := low
-	for j := low; j < high-1; j++ {
-		if (*A)[j] < pivot {
-			if i != j {
+func Swap(a []int, i int, j int) {
 
-			}
-		}
-	}
-	return i
+    a[i], a[j] = a[j], a[i]
 }
