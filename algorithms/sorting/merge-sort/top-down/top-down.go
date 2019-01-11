1a51dcd2b4c7387d8a67512c9205ff7450abd25c
gellel <lindsay.gelle@gmail.com>
2019-01-11 15:32:50 +1100

    trying merge

--- algorithms/sorting/merge-sort/top-down/top-down.go
+++ algorithms/sorting/merge-sort/top-down/top-down.go
@@ -8,6 +8,16 @@ func mergesort(a []int, b []int, n int) []int {
 
 }
 
+// Copy generates a copy of a Slice or Array.
+// Specifically copy moves a range of values from A into B.
+// The available range of values is set from the
+// provided range of floor to ceiling.
 func copy(a []int, b []int, floor int, ceiling int) []int {
-	
+    // iterate from lower bounds (defined by
+    // floor) until i reaches upper bounds
+    // (provided ceiling).
+    for i := floor; i < ceiling; i++ {
+        b[i] = a[i]
+    }
+    return b
 }
