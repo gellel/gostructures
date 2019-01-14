752abe89564b59b52aa7cc2ac1b3b42c55343e87
gellel <lindsay.gelle@gmail.com>
2019-01-14 13:16:55 +1100

    working merge top down

--- algorithms/sorting/merge-sort/top-down/top-down.go
+++ algorithms/sorting/merge-sort/top-down/top-down.go
@@ -1,45 +1,37 @@
 package topdown
 
-func MergeSort(a []int) []int {
-    return mergesort(a, make(int[], len(a) - 1), len(a) - 1)
+func Mergesort(a []int) []int {
+    return sort(a, clone(a), 0, len(a))
 }
 
-func mergesort(a []int, b []int, n int) []int {y
-    copy(a, b, 0, n)
-    split(a, b, 0, n)
-    return a
+func clone(a []int) []int {    
+    return append(make([]int, 0), a...)
 }
 
-func split(a int[], b int[], floor int, ceiling int) {
-    if (ceiling - floor) < 2 {
-        return
-    }
-    // similarly to Quicksort, create
-    // a partition that allows the recursive
-    // call to shift the ceiling and floor
-    // by 50% relative to the bounds
-    // of the passed sequence
-    // each time the is processed.
-    // i.e 1-through-4 becomes 1-through-2.
-    p := ((ceiling + floor) / 2)
-
-    split(a, b, floor, p)
-    split(a, b, p, ceiling)
-}
-
-func merge() {}
-
-
-// Copy generates a copy of a Slice or Array.
-// Specifically copy moves a range of values from A into B.
-// The available range of values is set from the
-// provided range of floor to ceiling.
-func copy(a []int, b []int, floor int, ceiling int) []int {
-    // iterate from lower bounds (defined by
-    // floor) until i reaches upper bounds
-    // (provided ceiling).
-    for i := floor; i < ceiling; i++ {
-        b[i] = a[i]
+func merge(a []int, b []int, floor int, partition int, ceiling int) []int {
+    i := floor
+    j := partition
+    
+    for k := floor; k < ceiling; k++ {
+        if (i < partition) && ((j >= ceiling) || (a[i] <= a[j])) {
+            b[k] = a[i]
+            i = i + 1
+        } else {
+            b[k] = a[j]
+            j = j + 1
+        }
     }
     return b
+}
+
+func sort(a []int, b []int, floor int, ceiling int) []int {
+    if !((ceiling - floor) < 2) {
+
+        partition := ((floor + ceiling) / 2)
+
+        sort(b, a, floor, partition)
+        sort(b, a, partition, ceiling)
+        merge(b, a, floor, partition, ceiling)
+    }
+    return a
 }
