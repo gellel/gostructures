cd59bc19b8d73582f7c474a2611254be53802a63
gellel <lindsay.gelle@gmail.com>
2019-01-15 00:17:40 +1100

    satisfying go export requirements

--- algorithms/sorting/merge-sort/merge-sort.go
+++ algorithms/sorting/merge-sort/merge-sort.go
@@ -1,9 +1,16 @@
 package mergesort
 
 import (
-    "github.com/gellel/gostructures/algorithms/sorting/merge-sort/top-down"
+	bottomup "github.com/gellel/gostructures/algorithms/sorting/merge-sort/bottom-up"
+	topdown "github.com/gellel/gostructures/algorithms/sorting/merge-sort/top-down"
 )
 
+// BottomUp merge-sort algorithm.
+func BottomUp(a []int) []int {
+	return bottomup.Mergesort(a)
+}
+
+// TopDown merge-sort algorithm
 func TopDown(a []int) []int {
 	return topdown.Mergesort(a)
 }
