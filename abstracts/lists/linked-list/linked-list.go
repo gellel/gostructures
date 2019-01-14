2b0f8d597d34183ee48c0867e060f3ca61bb770c
gellel <lindsay.gelle@gmail.com>
2019-01-15 09:59:10 +1100

    moving structures into a new dir

---
+++ abstracts/lists/linked-list/linked-list.go
@@ -0,0 +1,20 @@
+package linkedlist
+
+type LinkedList struct {}
+
+
+func (linkedList *LinkedList) Append(property interface{}) 	*LinkedList {
+
+}
+
+func (linkedlist *LinkedList) Delete(property interface{}) 	*LinkedList {
+
+}
+
+func (linkedlist *LinkedList) Prepend(property interface{}) *LinkedList {
+
+}
+
+func (linkedList *LinkedList) Search(property interface{}) 	*LinkedList {
+    
+}
