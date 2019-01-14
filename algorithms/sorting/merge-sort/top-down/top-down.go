aadf92cf5faab071b70352910f4256c5de27ec6a
gellel <lindsay.gelle@gmail.com>
2019-01-14 14:04:35 +1100

    working through comments to explain operations

--- algorithms/sorting/merge-sort/top-down/top-down.go
+++ algorithms/sorting/merge-sort/top-down/top-down.go
@@ -1,29 +1,54 @@
+// Package topdown provides Mergesort 
+// using the top-down merge sort scheme.
+// This implementation recursively subdivides
+// N number of sequences until subset reaches 
+// maximum a length of 1. These subsets
+// are then merged sequentially to create
+// the sorted sequence.
 package topdown
 
+// Mergesort algorithm using top-down merge sort scheme.
 func Mergesort(a []int) []int {
     return sort(a, clone(a), 0, len(a))
 }
 
+// Clone duplicates Slice or Array A
+// and spreads A's contents into Slice B.
+// Effectively builtins.copy but simplified
+// to a single returnable line.
 func clone(a []int) []int {    
     return append(make([]int, 0), a...)
 }
 
+// Merge performs the core top down merge sort method.
+// Merge takes sequence A and B and performs
+// re-ordering operations. Argument A is generally
+// provided as the temporary Array B in most
+// implementations but has been re-ordered here
+// for personal clarity. This helps identifying how
+// the original Slice or Array (A) is mutated across
+// function calls.
 func merge(a []int, b []int, floor int, partition int, ceiling int) []int {
+    //
     i := floor
+    //
     j := partition
     
     for k := floor; k < ceiling; k++ {
-        if (i < partition) && ((j >= ceiling) || (a[i] <= a[j])) {
-            b[k] = a[i]
+        // confirm i has not exceeded the provided
+        // upper bounds 
+        if (i < partition) && ((j >= ceiling) || (b[i] <= b[j])) {
+            a[k] = b[i]
             i = i + 1
         } else {
-            b[k] = a[j]
+            a[k] = b[j]
             j = j + 1
         }
     }
-    return b
+    return a
 }
 
+// Sort subsets
 func sort(a []int, b []int, floor int, ceiling int) []int {
     if !((ceiling - floor) < 2) {
 
@@ -31,7 +56,7 @@ func sort(a []int, b []int, floor int, ceiling int) []int {
 
         sort(b, a, floor, partition)
         sort(b, a, partition, ceiling)
-        merge(b, a, floor, partition, ceiling)
+        merge(a, b, floor, partition, ceiling)
     }
     return a
 }
