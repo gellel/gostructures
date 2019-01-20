822f3a60f4a524b32674065c90494a392e967799
gellel <lindsay.gelle@gmail.com>
2019-01-20 22:53:40 +1100

    saving

--- abstracts/trees/binary/binary-node/binary-node.go
+++ abstracts/trees/binary/binary-node/binary-node.go
@@ -1,15 +1,76 @@
 package node
 
+import "math"
+
 const (
-	LEFT  string = "LEFT"
+	// LEFT declares constant string that provides a namespace to annotate that a Binary-node is assigned as a left child of its parent.
+	LEFT string = "LEFT"
+	// RIGHT declares constant string that provides a namespace to annotate that a Binary-node is assigned as a right child of its parent.
 	RIGHT string = "RIGHT"
 )
+
+type binary interface {
+	EmptyLeft() bool
+	EmptyRight() bool
+	HasLeft() bool
+	HasRight() bool
+	Height() int
+	HeightOf() float64
+	HeightLeft() float64
+	HeightRight() float64
+	IsChild(b *Binary) bool
+	IsEqual(value float64) bool
+	IsLess(value float64) bool
+	IsMore(value float64) bool
+}
 
 type Binary struct {
 	Left  *Binary
 	Right *Binary
 	Side  string
 	Value float64
+}
+
+func (binary *Binary) EmptyLeft() bool {
+	return binary.Left == nil
+}
+
+func (binary *Binary) EmptyRight() bool {
+	return binary.Right == nil
+}
+
+func (binary *Binary) HasLeft() bool {
+	return binary.Left != nil
+}
+
+func (binary *Binary) HasRight() bool {
+	return binary.Right != nil
+}
+
+func (binary *Binary) Height() int {
+	return int(binary.HeightOf())
+}
+
+func (binary *Binary) HeightOf() float64 {
+	return math.Max(binary.HeightLeft(), binary.HeightRight())
+}
+
+func (binary *Binary) HeightLeft() float64 {
+	if binary.HasLeft() {
+		return (binary.Left.HeightOf() + 1.0)
+	}
+	return 0.0
+}
+
+func (binary *Binary) HeightRight() float64 {
+	if binary.HasRight() {
+		return (binary.Right.HeightOf() + 1.0)
+	}
+	return 0.0
+}
+
+func (binary *Binary) IsChild(b *Binary) bool {
+	return binary.Left == b || binary.Right == b
 }
 
 func (binary *Binary) IsEqual(value float64) bool {
@@ -31,3 +92,5 @@ func (binary *Binary) IsRight() bool {
 func (binary *Binary) IsMore(value float64) bool {
 	return binary.Value > value
 }
+
+var _ binary = (*Binary)(nil)
---
+++ abstracts/trees/binary/binary-node/binary-node_test.go
@@ -0,0 +1,12 @@
+package node_test
+
+import (
+	"fmt"
+	"testing"
+
+	node "github.com/gellel/gostructures/abstracts/trees/binary/binary-node"
+)
+
+func Test(t *testing.T) {
+	fmt.Println(node.New(1.0))
+}
