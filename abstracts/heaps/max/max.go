1e85711e20efe91529731ca3489d86cb87eefe90
gellel <lindsay.gelle@gmail.com>
2019-02-12 11:19:20 +1100

    stashing

--- abstracts/heaps/max/max.go
+++ abstracts/heaps/max/max.go
@@ -4,10 +4,14 @@ import "fmt"
 
 type heap interface {
 	Access(p int) uint
+	Append(value uint) *Heap
 	Bounds(p int) bool
 	Empty() *Heap
 	Fill(a []uint) *Heap
 	Filter(a []uint) []uint
+	IsEmpty() bool
+	IsLeaf(p int) bool
+	IsNotEmpty() bool
 	Left(p int) int
 	Length() int
 	Merge(h *Heap) *Heap
@@ -15,6 +19,7 @@ type heap interface {
 	Peek(p int) uint
 	PeekFirst() uint
 	PeekLast() uint
+	Push(value uint) int
 	Swap(i int, j int) *Heap
 	Violates(value uint) error
 }
@@ -25,6 +30,14 @@ type Heap []uint
 // Access reaches into Heap and attempts to read value at argument position, without checking whether the position is in Heap bounds.
 func (heap *Heap) Access(p int) uint {
 	return (*heap)[p]
+}
+
+// Append assigns a new unsigned integer to the end of the Heap, without bubbling the new value.
+func (heap *Heap) Append(value uint) *Heap {
+
+	*heap = append((*heap), value)
+
+	return heap
 }
 
 // Bounds returns a boolean checking whether the argument position does not overflow or underflow the current Heap length.
@@ -63,6 +76,21 @@ func (heap *Heap) Filter(a []uint) []uint {
 	return b
 }
 
+// IsEmpty checks the size of the Heap, determining whether it is empty or populated.
+func (heap *Heap) IsEmpty() bool {
+	return (heap.Length() == 0)
+}
+
+// IsLeaf checks the argument position is a leaf in the Heap.
+func (heap *Heap) IsLeaf(p int) bool {
+	return ((p >= (heap.Length() / 2)) && (p <= heap.Length()))
+}
+
+// IsNotEmpty checks the size of the Heap, checking whether it contains at least one element.
+func (heap *Heap) IsNotEmpty() bool {
+	return (heap.Length() > 0)
+}
+
 // Left calculates the left position in a binary search, relative to argument position.
 func (heap *Heap) Left(p int) int {
 	return p * 2
@@ -94,6 +122,14 @@ func (heap *Heap) PeekFirst() uint {
 // PeekLast attempts to access the unsigned integer stored at the end of the Heap; the heap-max-min.
 func (heap *Heap) PeekLast() uint {
 	return heap.Peek(heap.Length() - 1)
+}
+
+// Push adds a new unsigned integer into the Heap, then bubbles the value to the correct position before returning its index.
+func (heap *Heap) Push(value uint) int {
+
+	heap.Append(value)
+
+	return heap.Length() - 1
 }
 
 // Merge concatenates accessed Heap with argument Heap.
