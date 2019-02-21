cf6a266b40fc9ce115ed3df764014a199e39dfeb
gellel <lindsay.gelle@gmail.com>
2019-02-21 12:05:52 +1100

    wips

--- abstracts/heaps/heap/container/container.go
+++ abstracts/heaps/heap/container/container.go
@@ -1,8 +1,14 @@
 package container
 
 type container interface {
-
+	Peek() interface{}
+	PeekAt(p int) interface{}
+	PeekFirst() interface{}
+	PeekLast() interface{}
+	Poll() interface{}
+	Push(value interface{}) int
 }
 
-type Container []interface
+type Container []interface{}
 
+var _ container = (*Container)(nil)
---
+++ abstracts/heaps/heap/container/container_test.go
@@ -0,0 +1,13 @@
+package container_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/gellel/gostructures/abstracts/heaps/heap/container"
+)
+
+func Test(t *testing.T) {
+
+	fmt.Println(container.Container{})
+}
