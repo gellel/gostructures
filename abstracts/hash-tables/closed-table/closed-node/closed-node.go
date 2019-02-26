ec052e2dd4a24b5cd61ebb97a0e2073e60ee572f
gellel <lindsay.gelle@gmail.com>
2019-02-26 21:05:13 +1100

    stashing

--- abstracts/hash-tables/closed-table/closed-node/closed-node.go
+++ abstracts/hash-tables/closed-table/closed-node/closed-node.go
@@ -4,6 +4,21 @@ const (
 	SIZE int = 2
 )
 
-type closed interface{}
+type closed interface {
+	IsEmpty() bool
+	IsNotEmpty() bool
+	Length() int
+	PositionOf() int
+	RemoveValue() *Closed
+	SetValue(value interface{}) *Closed
+	ValueOf() string
+}
 
 type Closed [SIZE]interface{}
+
+func New(i int, value interface{}) *Closed {
+	return &Closed{i, value}
+}
+
+func (closed *Closed) PositionOf() int {
+}
--- abstracts/hash-tables/closed-table/closed-table.go
+++ abstracts/hash-tables/closed-table/closed-table.go
@@ -11,9 +11,13 @@ const (
 
 type table interface {
 	Add(key string, value interface{}) int
+	Append(n *node.Closed) int
 	Contains(key string) bool
+	Create() *single.LinkedList
 	Delete(key string) bool
 	Hash(key string) int
+	IsEmpty(p int) bool
+	IsNotEmpty(p int) bool
 	Length() int
 	LengthAt(p int) int
 	LengthBy(key string) int
@@ -21,5 +25,10 @@ type table interface {
 }
 
 type Table [SIZE]*single.LinkedList
+
+func (table *Table) Add(key string, value interface{}) int {
+	i := table.Hash(key)
+	n := node.Closed{i, value}
+}
 
 var _ table = (*Table)(nil)
