09eaffb002a665e889915af3a20062f62fd10630
Lindsay Gelle <lindsay.gelle@gmail.com>
2019-02-28 22:50:36 +1100

    Update continious-number.go

--- algorithms/other/continious-number/continious-number.go
+++ algorithms/other/continious-number/continious-number.go
@@ -1,10 +1,14 @@
 package continious
 
 const (
+	// R declares the constant numeric sum that is assigned when the iteration match condition does not match. 
 	R int = 1
+	// C declares the constant numeric sum the the sequence of repeating numbers begins at.
 	C int = 1
 )
 
+// Number iterates across slice, checking adjacent indexes in the argument slice and whether positions a[i] and a[i+1] are identical.
+// Counts the longest running sequence for n number of times this condition is satisfied.
 func Number(a []int) (int, int) {
 	i := 0
 	k := -1
@@ -18,7 +22,7 @@ func Number(a []int) (int, int) {
 			if r > c {
 				c = r
 			}
-			r = 0
+			r = R
 		}
 		i = i + 1
 	}
