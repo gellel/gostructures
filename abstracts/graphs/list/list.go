4b85cb3ce2169a549d89524c2dfe91e7b8ea9a07
Lindsay Gelle <lindsay.gelle@gmail.com>
2019-03-06 21:29:40 +1100

    Update list.go

--- abstracts/graphs/list/list.go
+++ abstracts/graphs/list/list.go
@@ -1,3 +1,7 @@
+// Package list exports an Adjacency-List Graph. Adjacency-List Graph manages
+// a collection of nodes and edges representing a connected series of data.
+// Graph vertices are stored structs of Linked-Lists, and every vertex stores a collection of
+// its of adjacent vertices.
 package list
 
 import list "github.com/gellel/gostructures/abstracts/graphs/list/list-linked"
