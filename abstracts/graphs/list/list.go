e7bf039b2dd4900f05bd0d45c3c8f25cd7375fc4
gellel <lindsay.gelle@gmail.com>
2019-03-07 20:48:30 +1100

    adding

--- abstracts/graphs/list/list.go
+++ abstracts/graphs/list/list.go
@@ -3,8 +3,7 @@ package list
 type graph interface {
 	AddEdge(source string, destination string) *Graph
 	AddVertex(source string) *Graph
-	EdgeLength(source string) int
-	HasEdge(source string)
+	HasVertex(source string)
 	IsConnected(source string, destination string) bool
 	IsEdgeEmpty(source string) bool
 	IsEdgeNotEmpty(source string) bool
@@ -12,7 +11,26 @@ type graph interface {
 	IsVertexNotEmpty(source string) bool
 	RemoveEdge(source string, destination string) *Graph
 	RemoveVertex(source string) *Graph
-	VertexLength(source string) int
 }
 
 type Graph map[string][]string
+
+func (graph *Graph) AddEdge(source string, destination string) *Graph {}
+
+func (graph *Graph) AddVertex(source string) *Graph {}
+
+func (graph *Graph) HasVertex(source string) bool {
+	_, ok := (*graph)[source]
+	return ok
+}
+
+func (graph *Graph) IsConnected(source string, destination string) bool {
+	if graph.HasVertex(source) {
+		for _, vertex := range (*graph)[source] {
+			if vertex == destination {
+				return true
+			}
+		}
+	}
+	return false
+}
