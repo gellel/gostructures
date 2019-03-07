4b6c309b2f37d81d57ed4b5d1d889deac6176a34
gellel <lindsay.gelle@gmail.com>
2019-03-07 21:19:24 +1100

    stashing

--- abstracts/graphs/list/list.go
+++ abstracts/graphs/list/list.go
@@ -1,36 +1,99 @@
 package list
 
 type graph interface {
-	AddEdge(source string, destination string) *Graph
-	AddVertex(source string) *Graph
-	HasVertex(source string)
-	IsConnected(source string, destination string) bool
-	IsEdgeEmpty(source string) bool
-	IsEdgeNotEmpty(source string) bool
-	IsVertexEmpty(source string) bool
-	IsVertexNotEmpty(source string) bool
-	RemoveEdge(source string, destination string) *Graph
-	RemoveVertex(source string) *Graph
+	AddEdge(source string, destination string) bool
+	AddVertex(vertex string) bool
+	Connected(source string, destination string) bool
+	GetEdges(vertex string) []string
+	GetVertices() []string
+	Length() int
+	LengthOf(vertex string) int
+	RemoveEdge(source string, destination string) bool
+	RemoveVertex(vertex string) bool
 }
 
 type Graph map[string][]string
 
-func (graph *Graph) AddEdge(source string, destination string) *Graph {}
-
-func (graph *Graph) AddVertex(source string) *Graph {}
-
-func (graph *Graph) HasVertex(source string) bool {
-	_, ok := (*graph)[source]
-	return ok
+func (graph *Graph) AddEdge(source string, destination string) bool {
+	g := *graph
+	if _, ok := g[source]; ok {
+		if _, ok := g[destination]; ok {
+			g[source] = append(g[source], destination)
+			return true
+		}
+	}
+	return false
 }
 
-func (graph *Graph) IsConnected(source string, destination string) bool {
-	if graph.HasVertex(source) {
-		for _, vertex := range (*graph)[source] {
-			if vertex == destination {
-				return true
+func (graph *Graph) AddVertex(vertex string) bool {
+	if _, ok := (*graph)[vertex]; ok == false {
+		(*graph)[vertex] = []string{}
+		return true
+	}
+	return false
+}
+
+func (graph *Graph) Connected(source string, destination string) bool {
+	g := *graph
+	if _, ok := g[source]; ok {
+		if _, ok := g[destination]; ok {
+			for _, vertex := range g[source] {
+				if vertex == destination {
+					return true
+				}
 			}
 		}
 	}
 	return false
 }
+
+func (graph *Graph) GetEdges(vertex string) []string {
+	if _, ok := (*graph)[vertex]; ok {
+		return (*graph)[vertex]
+	}
+	return nil
+}
+
+func (graph *Graph) GetVertices() []string {
+	vertices := []string{}
+	for key := range *graph {
+		vertices = append(vertices, key)
+	}
+	return vertices
+}
+
+func (graph *Graph) Length() int {
+	return len(*graph)
+}
+
+func (graph *Graph) LengthOf(vertex string) int {
+	if _, ok := (*graph)[vertex]; ok {
+		return len((*graph)[vertex])
+	}
+	return -1
+}
+
+func (graph *Graph) RemoveEdge(source string, destination string) bool {
+	g := *graph
+	if _, ok := g[source]; ok {
+		if _, ok := g[destination]; ok {
+			for i, vertex := range g[source] {
+				if vertex == destination {
+					g[source] = append(g[source][:i], g[source][i+1:]...)
+					return true
+				}
+			}
+		}
+	}
+	return false
+}
+
+func (graph *Graph) RemoveVertex(source string) bool {
+	if _, ok := (*graph)[source]; ok {
+		delete((*graph), source)
+		return true
+	}
+	return false
+}
+
+var _ graph = (*Graph)(nil)
