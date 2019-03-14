4b0cc65dfb941fab4a75d48a5d5b7b39e262d45b
gellel <lindsay.gelle@gmail.com>
2019-03-14 18:59:27 +1100

    stashing

--- abstracts/graphs/matrix/matrix.go
+++ abstracts/graphs/matrix/matrix.go
@@ -1,5 +1,30 @@
 package matrix
 
-type graph interface{}
+type Graph [][]int
 
-type Graph [][]int{}
+func (graph *Graph) AddEdge(i int, j int) bool {
+
+}
+
+func (graph *Graph) HasCoordinate(i int, j int) bool {
+	return graph.HasRowAt(i) && graph.HasColumnAt(j)
+}
+
+func (graph *Graph) HasColumnAt(j int) bool {
+	return (j > -1) && (len(*graph) > 0) && (j < len((*graph)[0]))
+}
+
+func (graph *Graph) HasRowAt(i int) bool {
+	return (i > -1) && (i < len(*graph))
+}
+
+func (graph *Graph) Length() int {
+	return len(*graph)
+}
+
+func (graph *Graph) LengthOf(j int) int {
+	if graph.HasColumnAt(j) {
+		return len((*graph)[j])
+	}
+	return -1
+}
